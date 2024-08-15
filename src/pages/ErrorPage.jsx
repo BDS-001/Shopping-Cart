d33bf469abc505da0db2b4de8318a1a233ca16e1
BDS <113209967+BDS-001@users.noreply.github.com>
2024-08-15 17:33:21 -0400

    styled error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom"
+import '../styles/ErrorPage.css'
 
 function ErrorPage() {
     return (
         <>
-            <div>This Page does not exist, return to the <Link to={'/'}>Home Page</Link></div>
+            <div className="error"><h1>This Page does not exist, return to the <Link to={'/'}>Home Page</Link></h1></div>
         </>
     )
 }
