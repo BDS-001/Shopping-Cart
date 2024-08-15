02da85726440fd815fdf9627b2f676c4973f2cbc
BDS <113209967+BDS-001@users.noreply.github.com>
2024-08-15 19:09:59 -0400

    testing fake store api

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,13 @@
+function test() {
+    fetch('https://fakestoreapi.com/products/categories')
+            .then(res=>res.json())
+            .then(json=>console.log(json))
+}
+
 function Homepage() {
     return (
         <>
+            {test()}
             <div>Homepage</div>
         </>
     )
