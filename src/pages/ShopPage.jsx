2ed0cb7eecc2d1743b2d50733ee047fa5293133b
BDS <113209967+BDS-001@users.noreply.github.com>
2024-08-16 19:26:41 -0400

    setup shop page catalogue

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,7 +1,18 @@
+import { useOutletContext } from "react-router-dom";
+import Card from "../components/ProductCard";
+import '../styles/ShopPage.css'
+
 function ShopPage() {
+    const { products } = useOutletContext();
+
     return (
         <>
-            <div>ShopPage</div>
+            <h1>Shop Page</h1>
+            <div className="catalogue">
+                {products.map(product => (
+                    <Card key={product.id} imageUrl={product.image} title={product.title} description={product.description} />
+                ))}
+            </div>
         </>
     )
 }
