2a3fe38546d2f6883944b76f9d47ba53b87ff4f6
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-08-20 19:14:46 -0400

    updated error apge style

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,12 +1,19 @@
-import { Link } from "react-router-dom"
-import '../styles/ErrorPage.css'
+import { Link } from "react-router-dom";
+import '../styles/ErrorPage.css';
 
 function ErrorPage() {
     return (
-        <>
-            <div className="error"><h1>This Page does not exist, return to the <Link to={'/'}>Home Page</Link></h1></div>
-        </>
-    )
+        <div className="error">
+            <h1>
+                This Page does not exist, return to the{' '}
+                <Link to="/">
+                    <span style={{ color: 'lightblue' }}>
+                        Home Page
+                    </span>
+                </Link>
+            </h1>
+        </div>
+    );
 }
 
-export default ErrorPage
+export default ErrorPage;
