a5ffb44d69264efd90b3a0e43e602704115d1494
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-08-20 18:16:28 -0400

    product card can now adjust amount and add to cart

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,29 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+import "../styles/ProductCard.css"
 
+const handleCountChange = (e) => {
+  const value = parseInt(e.target.value, 10);
+  setItemCount(value >= 1 ? value : 1);
+};
+
+const handleAddToCart = () => {
+  console.log(`Adding ${itemCount} of ${title} to cart`);
+};
 
 const Card = ({ imageUrl, title, description }) => {
+  const [itemCount, setItemCount] = useState(1)
+
   return (
     <div className="card">
       <img src={imageUrl} alt={title} className="card-image" />
       <div className="card-content">
         <h2 className="card-title">{title}</h2>
         <p className="card-description">{description}</p>
+      </div>
+      <div className="item-purchase">
+        <input type="number" value={itemCount} min={1} onChange={handleCountChange} />
+        <button>Add To Cart</button>
       </div>
     </div>
   );
