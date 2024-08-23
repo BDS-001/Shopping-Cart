6bdb5af79cfe1e345c3b43eceadf9c311ee2d43b
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-08-23 19:01:16 -0400

    empty cart now displays message

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,15 +1,32 @@
 import { useOutletContext } from "react-router-dom";
 import CartItem from "../components/CartItem";
+import { Link } from "react-router-dom";
+import '../styles/CartPage.css'
 
 function Cart() {
-    const { cart, handleQuantityChange, handleRemoveFromCart } = useOutletContext();
+    const { cart, cartCount, handleQuantityChange, handleRemoveFromCart } = useOutletContext();
 
     return (
         <>
-        <h1>Cart</h1>
-        {cart.map(item => (
-            <CartItem key={item.id} item={item} handleQuantityChange={handleQuantityChange} handleRemoveFromCart={handleRemoveFromCart} />
-        ))}
+        {cartCount < 1 ? (
+                <div className="empty">
+                <h1>
+                    Your Cart is Empty! go to{' '}
+                    <Link to="/">
+                        <span style={{ color: 'lightblue' }}>
+                            Shopping Page
+                        </span>
+                    </Link>
+                    {' '}to get some items!
+                </h1>
+            </div> ) : (
+                <>
+                    <h1>Cart</h1>
+                    {cart.map(item => (
+                        <CartItem key={item.id} item={item} handleQuantityChange={handleQuantityChange} handleRemoveFromCart={handleRemoveFromCart} />
+                    ))}
+                </>
+                    )}
         </>
     )
 }
