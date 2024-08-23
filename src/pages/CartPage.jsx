fe3ef60fd65a1b1d35286ee703bb996ed6d5381e
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-08-23 19:20:28 -0400

    cart calculates cost

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -5,7 +5,8 @@ import '../styles/CartPage.css'
 
 function Cart() {
     const { cart, cartCount, handleQuantityChange, handleRemoveFromCart } = useOutletContext();
-
+    let subtotal = 0
+    if (cart.length > 0) subtotal = cart.reduce((total, item) => total + (item.productData.price * item.amount), 0)
     return (
         <>
         {cartCount < 1 ? (
@@ -22,9 +23,18 @@ function Cart() {
             </div> ) : (
                 <>
                     <h1>Cart</h1>
-                    {cart.map(item => (
-                        <CartItem key={item.id} item={item} handleQuantityChange={handleQuantityChange} handleRemoveFromCart={handleRemoveFromCart} />
-                    ))}
+                    <div className="cart-container">
+                        <div className="cart-container-content">
+                            {cart.map(item => (
+                                <CartItem key={item.id} item={item} handleQuantityChange={handleQuantityChange} handleRemoveFromCart={handleRemoveFromCart} />
+                            ))}
+                            <div className="payment">
+                                <p>Subtotal: {subtotal.toFixed(2)}</p>
+                                <p>HST(13%): {(subtotal * .13).toFixed(2)}</p>
+                                <p>Total: {(subtotal * 1.13).toFixed(2)}</p>
+                            </div>
+                        </div>
+                    </div>
                 </>
                     )}
         </>
