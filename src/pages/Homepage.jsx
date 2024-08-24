8215655608dc7416b3d0a5d95f18c460b6a82c99
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-08-24 16:57:08 -0400

    planned out homepage features

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,11 @@
 function Homepage() {
     return (
         <>
-            <div>Homepage</div>
+            <h1>Welcome To shopping Site</h1>
+            {/* search bar */}
+            {/* products carousel */}
+            {/* new arrivals / best sellers section */}
+            {/* newsletter signup */}
         </>
     )
 }
